b143c81f42d6a3aec2f4b723cd58c1fddddcae1a
Josuebmota <josuebatistam1@gmail.com>
2021-03-18 20:47:38 -0300

    feature/deployment: configurando cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
 
@@ -15,6 +16,7 @@ import '@shared/container';
 const app = express();
 
 // app.use(rateLimiter);
+app.use(cors());
 app.use(express.json());
 app.use('/onicadastroapi/v1/file', express.static(uploadConfig.uploadsFolder));
 app.use('/onicadastroapi/v1/', routes);
